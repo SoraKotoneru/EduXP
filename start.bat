050372786ec43e065dc0529dc53bcf75724fa534
SoraK <kotoneru21@mail.ru>
2025-06-20 15:03:24 +0500

    обнова 4 дня

--- start.bat
+++ start.bat
@@ -1,15 +1,21 @@
 @echo off
 REM Скрипт запуска сервера и открытия страницы в браузере
 
+REM Определяем путь к npm
+set "NPM_CMD=%ProgramFiles%\nodejs\npm.cmd"
+if not exist %NPM_CMD% (
+  set "NPM_CMD=npm.cmd"
+)
+
 REM Переходим в папку server и устанавливаем зависимости при необходимости
-pushd %~dp0server
+pushd %~dp0\server
 if not exist node_modules (
   echo Installing server dependencies...
-  npm install
+  %NPM_CMD% install
 )
 
 echo Starting server with hot reload...
-start "EduXP Server" cmd /k "npm run dev"
+start "EduXP Server" cmd /k "%NPM_CMD% run dev"
 popd
 
 REM Ожидаем несколько секунд, чтобы сервер успел запуститься
